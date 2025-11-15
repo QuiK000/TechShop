9be2f1aa518708bf05bd1697b09b9480adb15fc0
QuiK000 <teamquiksteam@gmail.com>
2025-11-15 18:58:28 +0200

    ...

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5606e2995ff171f0a40402fe222095b276cac4d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d16a0d210d9124d833e30b599b6db7a3f0b33af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
