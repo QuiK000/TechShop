3d477daf7658eceddae809e5c77017601d0d83bb
QuiK000 <teamquiksteam@gmail.com>
2025-11-15 14:08:13 +0200

    ...

---
+++ Views/Order/Checkout.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Views.Order;
+
+public class Checkout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab47334c0f0f6391216817e71d3c5be0ff52c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
